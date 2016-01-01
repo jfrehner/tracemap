a740638c0663b20b6ef1e66b1f44f7ad569d8514
AndreasGassmann <andreas@andreasgassmann.ch>
2016-01-01 23:53:39 +0100

    - Fixed: switch from id to class

--- templates/index.php
+++ templates/index.php
@@ -31,7 +31,7 @@
         <header>
             <hgroup>
                 <h1><?php echo $this->data['page_title']; ?></h1>
-                <h2>We traced <span id="numberOfTraces"></span> Routes so far.</h2>
+                <h2>We traced <span class="numberOfTraces"></span> Routes so far.</h2>
             </hgroup>
         </header>
 
